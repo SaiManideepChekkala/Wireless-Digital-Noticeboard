00b67195d484e5eb6c0675412452e55fec43916f
Sai Manideep Chekkala <saimanideep3101@gmail.com>
2025-08-04 21:01:16 +0530

    Delete 74LS164.h

--- 74LS164.h
+++
@@ -1,9 +0,0 @@
-void Init_74LS164(void);
-
-void SIPO_74LS164_1(unsigned char );//Serial In Parallel Out(SIPO) Operations
-
-void SIPO_74LS164_2(unsigned char );
-
-void SIPO_74LS164_3(unsigned char );
-
-void SIPO_74LS164_4(unsigned char );
